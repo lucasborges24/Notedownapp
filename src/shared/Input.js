ba2c70736c17443dacefdf271186557aa45fd247
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-15 12:39:45 -0300

    feat: create Input shared component

---
+++ src/shared/Input.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+function Input() {
+  return <></>;
+}
+
+function Label() {
+  return <></>;
+}
+
+export { Input, Label };
